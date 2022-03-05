080d9e829335ce9724725a5bff3ab9091400d1e4
shahriarRahmanShaon <shaon463@gmail.com>
2022-03-06 00:43:23 +0600

    Initial Commit

---
+++ ElegentCalender/ElegentCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElegentCalenderApp.swift
+//  ElegentCalender
+//
+//  Created by sergio shaon on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ElegentCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
